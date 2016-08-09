0d10efaf3135d3776fc166b61a98d37276e8f8ab
JS <js@johnsiu.com>
2016-08-09 09:16:00 -0400

    Pass message between component one and two

--- app/one.component.ts
+++ app/one.component.ts
@@ -5,24 +5,34 @@ import {SimpleMQ} from 'ng2-simple-mq';
 	'selector': 'one-component',
 	'template': `
 		<h3>{{title}}</h3>
+		<div>Send message to component two <input [(ngModel)]="msgTwo"><button (click)="sendToTwo()">Send</button></div>
+		<div>Broadcast message <input [(ngModel)]="msgBroadcast"><button (click)="broadcast()">Broadcast</button></div>
 		<div>Receive from queue 'one' : {{msg}}</div>
 		<div>Receive from queue 'broadcast' : {{broadcastMsg}}</div>`
 })
 export class OneComponent implements OnInit {
 	title = 'Component One';
 	msg;
+	msgTwo;
+	msgBroadcast;
 	broadcastMsg;
 	constructor(private smq: SimpleMQ) { }
 	ngOnInit() {
 		this.smq.subscribe('one', e => this.receiveMsg(e));
 		this.smq.subscribe('broadcast', e => this.receiveBroadcast(e));
 	}
-
+	sendToTwo() {
+		// Publish to queue name 'two'
+		this.smq.publish('two', this.msgTwo);
+	}
+	broadcast() {
+		// Publish to queue name 'broadcast'
+		this.smq.publish('broadcast', this.msgBroadcast);
+	}
 	receiveMsg(m) {
 		this.msg = m;
 		console.log('1:' + this.msg);
 	}
-
 	receiveBroadcast(m) {
 		this.broadcastMsg = m;
 		console.log('1bc:' + this.broadcastMsg);
--- app/two.component.ts
+++ app/two.component.ts
@@ -5,24 +5,34 @@ import {SimpleMQ} from 'ng2-simple-mq';
 	'selector': 'two-component',
 	'template': `
 		<h3>{{title}}</h3>
+		<div>Send message to component two <input [(ngModel)]="msgTwo"><button (click)="sendToOne()">Send</button></div>
+		<div>Broadcast message <input [(ngModel)]="msgBroadcast"><button (click)="broadcast()">Broadcast</button></div>
 		<div>Receive from queue 'two' : {{msg}}</div>
 		<div>Receive from queue 'broadcast' : {{broadcastMsg}}</div>`
 })
 export class TwoComponent implements OnInit {
 	title = 'Component Two';
 	msg;
+	msgOne;
+	msgBroadcast;
 	broadcastMsg;
 	constructor(private smq: SimpleMQ) { }
 	ngOnInit() {
 		this.smq.subscribe('two', e => this.receiveMsg(e));
 		this.smq.subscribe('broadcast', e => this.receiveBroadcast(e));
 	}
-
+	sendToOne() {
+		// Publish to queue name 'two'
+		this.smq.publish('two', this.msgOne);
+	}
+	broadcast() {
+		// Publish to queue name 'broadcast'
+		this.smq.publish('broadcast', this.msgBroadcast);
+	}
 	receiveMsg(m) {
 		console.log('2:' + m);
 		this.msg = m;
 	}
-
 	receiveBroadcast(m) {
 		console.log('2bc:' + m);
 		this.broadcastMsg = m;
